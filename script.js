24f087ad0118cd949f338b7c1a7792c0cf5d6cb0
RichardKingg <ricardo.elizondo.jau@gmail.com>
2022-12-16 20:31:00 -0600

    Add function for the game board

---
+++ script.js
@@ -0,0 +1,17 @@
+//Module for creating the game board
+let gameBoard = (function () {
+  let container = document.createElement("div");
+  container.classList.add("board");
+  let gameArray = [];
+  const rows = 3;
+  const cols = 3;
+  for (let i = 0; i < rows; i++) {
+    gameArray[i] = [];
+    for (let j = 0; j < cols; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("boardCell");
+      grid[i][j] = cell;
+      container.appendChild(grid[i][j]);
+    }
+  }
+})();
