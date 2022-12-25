5abe6017b017aa5eccaa6e795329776dd83b1174
RichardKingg <ricardo.elizondo.jau@gmail.com>
2022-12-25 12:39:46 -0600

    - Rework gameBoard module in script.js to map the grid values.
    - Create a 3*3 grid on index.html.
    
    - Change gameBoard at styles.css display to grid.

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 //Module for creating the cells in the game board
 let gameBoard = (function () {
   let container = document.querySelector(".gameBoard");
-  let gameArray = [];
-  for (let i = 0; i < 3; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < 3; j++) {
-      const cell = document.createElement("div");
-      cell.classList.add("boardCell");
-      row.appendChild(cell);
-    }
-    container.appendChild(row);
-  }
+  let gameArray = [[0, 1, 2][(3, 4, 5)][(6, 7, 8)]];
   return {
     container,
     gameArray,
